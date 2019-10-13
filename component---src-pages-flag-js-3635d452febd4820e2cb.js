eeadb37dbff763f4f8f5346b730c73a93964c7c0
Joseph Surin <joseph.surin@gmail.com>
2019-10-13 16:10:05 +1100

    Updates

---
+++ component---src-pages-flag-js-3635d452febd4820e2cb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{312:function(e,n,a){"use strict";a.r(n);var t=a(1),l=a.n(t);n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("center",null,l.a.createElement("div",{style:{width:"600px",marginTop:"300px"}},"ok fine you can have a flag... ",l.a.createElement("code",null,"MISCWEBCTF{it_wont_always_be_this_easy}"))))}}}]);
+//# sourceMappingURL=component---src-pages-flag-js-3635d452febd4820e2cb.js.map
