fbd77874fbd7107979e82f877d1b0c88869ce3b7
Joseph Surin <joseph.surin@gmail.com>
2019-11-23 15:02:05 +1100

    Updates

--- component---src-pages-flag-js-de9e08ecf588fc283740.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{324:function(e,n,a){"use strict";a.r(n);var t=a(1),l=a.n(t);n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("center",null,l.a.createElement("div",{style:{width:"600px",marginTop:"300px"}},"ok fine you can have a flag... ",l.a.createElement("code",null,"MISCWEBCTF{it_wont_always_be_this_easy}"))))}}}]);
-//# sourceMappingURL=component---src-pages-flag-js-de9e08ecf588fc283740.js.map
