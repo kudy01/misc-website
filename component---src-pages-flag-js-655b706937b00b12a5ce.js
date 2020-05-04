0343956088e497bf29195785fbe150892790edfc
fumetsuu <joseph.surin@gmail.com>
2020-05-04 15:24:39 +1000

    Updates

---
+++ component---src-pages-flag-js-655b706937b00b12a5ce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{329:function(e,n,a){"use strict";a.r(n);var t=a(0),l=a.n(t);n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("center",null,l.a.createElement("div",{style:{width:"600px",marginTop:"300px"}},"ok fine you can have a flag... ",l.a.createElement("code",null,"MISCWEBCTF{it_wont_always_be_this_easy}"))))}}}]);
+//# sourceMappingURL=component---src-pages-flag-js-655b706937b00b12a5ce.js.map
